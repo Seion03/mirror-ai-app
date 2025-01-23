16c363f7c09a78954ed62082d207395c3621f782
AlenPAji <99082266+AlenPAji@users.noreply.github.com>
2025-01-23 14:15:22 +0530

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,103 +1,67 @@
-import { StyleSheet } from 'react-native';
-import {
-    Camera,
-    useCameraDevice,
-    useCameraPermission,
-    useFrameProcessor,
-} from 'react-native-vision-camera';
-import { useEffect, useRef } from 'react';
-import * as ImageManipulator from 'expo-image-manipulator';
-import { useResizePlugin } from 'vision-camera-resize-plugin';
-import { io, Socket } from "socket.io-client";
+import React, { useRef, useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import {ApiVideoLiveStreamMethods, ApiVideoLiveStreamView} from '@api.video/react-native-livestream';
 
-import {runOnJS} from "react-native-reanimated";
-
-
-function PermissionsPage() {
-    return null;
-}
-function NoCameraDeviceError() {
-    return null;
-}
-
-export default function HomeScreen() {
-    const device = useCameraDevice('back');
-    const { hasPermission } = useCameraPermission();
-    const { resize } = useResizePlugin();
-
-    const socketRef = useRef<Socket | null>(null);
-
-    useEffect(() => {
-        askPermission();
-
-        const socket = io('http://192.168.1.4:8765');
-        socketRef.current = socket;
-
-        socket.on('connect', () => {
-            console.log('Connected to server');
-        });
-        // Replace with your server IP
-
-
-        return () => {
-            socket.disconnect();// Clean up WebSocket connection when component unmounts
-        };
-    }, []);
-
-    const askPermission = async () => {
-        await Camera.requestCameraPermission();
-        await Camera.requestMicrophonePermission();
-    };
-
-    if (!hasPermission) return <PermissionsPage />;
-    if (!device) return <NoCameraDeviceError />;
-
-    const frameProcessor = useFrameProcessor((frame) => {
-        'worklet';
-
-        // Resize the frame
-        const resized = resize(frame, {
-            scale: {
-                width: 192,
-                height: 192,
-            },
-            pixelFormat: 'rgb',
-            dataType: 'uint8',
-        });
-
-        // Convert the resized frame to a Uint8Array (raw RGB bytes)
-        const byteArray = new Uint8Array(resized);
-
-        // Call a JS function using runOnJS
-        runOnJS(() => {
-            console.log('Frame processed and sending to server');
-
-            // Send the frame data to the server via WebSocket
-            if (socketRef.current ) {
-                socketRef.current.emit('frame', byteArray);
-            } else {
-                console.warn('Socket is not connected.');
-            }
-        })();
-    }, []);
-
-
-    const sendToServer = (byteArray: Uint8Array) => {
-        // Ensure socket is connected
-        if (socketRef.current ) {
-            socketRef.current.emit('frame', byteArray);
-        } else {
-            console.warn('Socket is not connected.');
-        }
-    };
+const App = () => {
+    const ref = React.useRef<ApiVideoLiveStreamMethods | null>(null);
+    const [streaming, setStreaming] = useState(false);
 
     return (
-        <Camera
-            style={StyleSheet.absoluteFill}
-            device={device}
-            isActive={true}
-            frameProcessor={frameProcessor}
-            // Adjust FPS if needed
-        />
+        <View style={{ flex: 1, alignItems: 'center' }}>
+            <ApiVideoLiveStreamView
+                style={{ flex: 1, backgroundColor: 'black', alignSelf: 'stretch' }}
+                ref={ref}
+                camera="back"
+                enablePinchedZoom={true}
+                video={{
+                    fps: 30,
+                    resolution: '720p', // Alternatively, you can specify the resolution in pixels: { width: 1280, height: 720 }
+                    bitrate: 2*1024*1024, // # 2 Mbps
+                    gopDuration: 1, // 1 second
+                }}
+                audio={{
+                    bitrate: 128000,
+                    sampleRate: 44100,
+                    isStereo: true,
+                }}
+                isMuted={false}
+                onConnectionSuccess={() => {
+                    //do what you want
+                }}
+                onConnectionFailed={(e) => {
+                    //do what you want
+                }}
+                onDisconnect={() => {
+                    //do what you want
+                }}
+            />
+            <View style={{ position: 'absolute', bottom: 40 }}>
+                <TouchableOpacity
+                    style={{
+                        borderRadius: 50,
+                        backgroundColor: streaming ? 'red' : 'white',
+                        width: 50,
+                        height: 50,
+                    }}
+                    onPress={() => {
+                        if (streaming) {
+                            ref.current?.stopStreaming();
+                            setStreaming(false);
+                        } else {
+                            try {
+                                ref.current?.startStreaming('vwtr-zqsx-wt1x-c450-59hy','rtmp://a.rtmp.youtube.com/live2');
+                                setStreaming(true);
+                            }
+                            catch (e) {
+                                console.error(e);
+                            }
+
+                        }
+                    }}
+                />
+            </View>
+        </View>
     );
 }
+
+export default App;
